49cfabdb8907cfe56e693ca79b49b3d1d501ead8
Aleksandr Guzeev <aleksandr.guzeeff@yandex.ru>
2022-11-22 15:09:27 +0300

    Change first task

--- task.js
+++ task.js
@@ -1,11 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = false;
 
-  if (arr1.every((n, i) => n === arr2[i])) { 
-  result = true;
-   } else {
-    result = false;
-   }
+  if (arr1.length !== arr2.length)  return result;
+  if (arr1.every((n, i) => n === arr2[i])) result = true;
+
+  //if (arr1.every((n, i) => n === arr2[i])) && (arr1.length === arr2.length) { 
+  //result = true;
+  // } Почему здесь "&&" не работает?
 
   return result; // boolean
 }
